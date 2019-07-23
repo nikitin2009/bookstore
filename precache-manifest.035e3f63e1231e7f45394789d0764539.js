182337da70117f360144a8fe7fc8cebd96214eed
Alex N <nikitin2009@gmail.com>
2019-07-23 10:26:29 +0300

    Updates

---
+++ precache-manifest.035e3f63e1231e7f45394789d0764539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665b4ecff6529ed4df42c7aa51660819",
+    "url": "/bookstore/index.html"
+  },
+  {
+    "revision": "c5f6dce1a104c6ad660c",
+    "url": "/bookstore/static/css/main.21f2c1e2.chunk.css"
+  },
+  {
+    "revision": "dd4f0a9f4e4fb52ca272",
+    "url": "/bookstore/static/js/2.71a221f8.chunk.js"
+  },
+  {
+    "revision": "c5f6dce1a104c6ad660c",
+    "url": "/bookstore/static/js/main.7aa942b7.chunk.js"
+  },
+  {
+    "revision": "e3e84e75878dc5d45b9e",
+    "url": "/bookstore/static/js/runtime~main.2e2d1535.js"
+  }
+]);
